f0ab0ef1d3f7480d8acc81c7428761c58c37952a
duellal <alexandriaduell@gmail.com>
2022-12-15 15:36:08 -0500

    actions router + middleware complete

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,57 @@
 // add middlewares here related to actions
+const Actions = require(`./actions-model`)
+const Projects = require(`../projects/projects-model`)
+
+async function validateActionId(req, res, next) {
+   try {
+      const { id } = req.params
+      const action = await Actions.get(id)
+
+      if (action) {
+         req.action = action
+         next()
+      }
+      else {
+         next({
+            status: 404,
+            message: `action with id ${id} does not exist`
+         })
+      }
+   }
+   catch (err) {
+      next(err)
+   }
+}
+
+async function validateAction(req, res, next) {
+   const { project_id, description, notes } = req.body
+
+   const project_id_boolean = await Projects.get(project_id)
+
+   if (project_id_boolean) {
+      if (!project_id || !description || !notes) {
+         next({
+            status: 404,
+            message: `Something is missing. You either forgot the project id, the description of the action, or the notes for the action`
+         })
+      }
+      else {
+         next()
+      }
+   }
+   else {
+      next({
+         status: 404,
+         message: `Project with id ${project_id} does not exist.`
+      })
+   }
+}
+
+async function validateProjectIdAction(req, res, next) {
+
+}
+
+module.exports = {
+   validateActionId,
+   validateAction
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,11 +1,50 @@
 // Write your "actions" router here!
 const express = require('express')
 const Actions = require(`./actions-model`)
-//middleware here
+const { validateActionId, validateAction } = require(`./actions-middlware`)
 
 const router = express.Router()
 
 
+router.get(`/actions`, (req, res, next) => {
+   Actions.get()
+      .then(actions => {
+         res.status(200).json(actions)
+      })
+      .catch(next)
+})
+
+router.get(`/actions/:id`, validateActionId, (req, res, next) => {
+   Actions.get(req.params.id)
+      .then(action => {
+         res.status(200).json(action)
+      })
+      .catch(next)
+})
+
+router.post(`/actions`, validateAction, (req, res, next) => {
+   Actions.insert(req.body)
+      .then(action => {
+         res.status(200).json(action)
+      })
+      .catch(next)
+})
+
+router.put(`/actions/:id`, validateActionId, validateAction, (req, res, next) => {
+   Actions.update(req.params.id, req.body)
+      .then(action => {
+         res.status(200).json(action)
+      })
+      .catch(next)
+})
+
+router.delete(`/actions/:id`, validateActionId, (req, res, next) => {
+   Actions.remove(req.params.id)
+      .then(action => {
+         res.status(200).json()
+      })
+      .catch(next)
+})
 
 
 module.exports = router
